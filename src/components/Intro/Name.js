460eda607787351251d4cd8749651939632ec3c8
Nate St. Germain <nstgerma@uoguelph.ca>
2019-12-28 14:23:45 -0500

    adding name

---
+++ src/components/Intro/Name.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { CSSTransition } from "react-transition-group";
+
+import "../../assets/styles/Intro/Name.scss";
+
+const Name = () => {
+  return (
+    <div className="name">
+      <CSSTransition appear classNames="sliding-names" in timeout={800}>
+        <div>
+          <div className="first-name">NATE</div>
+          <div className="last-name">st. germain</div>
+        </div>
+      </CSSTransition>
+    </div>
+  );
+};
+
+export default Name;
--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { CSSTransition } from "react-transition-group";
+
 import Spinner from "./Spinner";
+import Name from "./Name";
 
 import "../../assets/styles/Intro/Intro.scss";
 
 const Intro = () => (
   <div className="intro">
     <Spinner />
+    <CSSTransition appear classNames="intro__vertical-divider" in timeout={600}>
+      <div />
+    </CSSTransition>
+    <Name />
   </div>
 );
 
