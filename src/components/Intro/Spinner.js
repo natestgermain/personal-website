d20751d3d926018763f08c62545a8f676e4b9559
Nate St. Germain <nstgerma@uoguelph.ca>
2019-12-28 20:54:39 -0500

    core of intro done

--- src/components/Intro/Spinner.js
+++ src/components/Intro/Spinner.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { CSSTransition } from "react-transition-group";
 
+import CloseupSquare from "../../assets/images/closeup-square.JPG";
+
 import "../../assets/styles/Intro/Spinner.scss";
 
 const Spinner = () => {
   return (
-    <CSSTransition appear classNames="spinner" in timeout={850}>
-      <div />
+    <CSSTransition appear classNames="spinner" in timeout={1000}>
+      <div className="spinner">
+        <img alt="Closeup of Nate" className="closeup" src={CloseupSquare} />
+      </div>
     </CSSTransition>
   );
 };
